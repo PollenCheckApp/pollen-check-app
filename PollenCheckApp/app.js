d0557e9c1604e92bffb4fb81320041a90794fc57
John Doe <gabiak+stack@gmail.com>
2020-07-07 13:39:37 +0200

    2nd day

--- PollenCheckApp/app.js
+++ PollenCheckApp/app.js
@@ -8,17 +8,11 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-const flash        = require("connect-flash");
 
-// packages for Signup, Log-in, Log-out
-const session = require("express-session");
-const bcrypt = require("bcrypt");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-
+// Mongoose configuration
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/pollencheckapp', {useNewUrlParser: true})
+  .connect('mongodb://localhost/gabiakpollenapp', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -36,43 +30,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-// Passport Middleware Setup 
-app.use(session({
-  secret: "our-passport-local-strategy-app",
-  resave: true,
-  saveUninitialized: true
-}));
-
-
-// Passport Middleware Initialization
-passport.serializeUser((user, cb) => {
-  cb(null, user._id);
-});
-passport.deserializeUser((id, cb) => {
-  User.findById(id, (err, user) => {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
-});
-passport.use(new LocalStrategy((username, password, next) => {
-  User.findOne({ username }, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return next(null, false, { message: "Incorrect username" });
-    }
-    if (!bcrypt.compareSync(password, user.password)) {
-      return next(null, false, { message: "Incorrect password" });
-    }
-    return next(null, user);
-  });
-}));
-
-app.use(passport.initialize()); 
-app.use(passport.session());
-
 
 // Express View engine setup
 
@@ -93,30 +50,13 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-// Mongoose configuration
-// mongoose.Promise = Promise;
-// mongoose
-// .connect('mongodb://localhost/basic-auth', {useMongoClient: true})
-// .then(() => {
-// console.log('Connected to Mongo!')
-// }).catch(err => {
-// console.error('Error connecting to mongo', err)
-// });
 
-// Connection to Routes
+
 const index = require('./routes/index');
 app.use('/', index);
 
-const router = require("./routes/auth-routes"); 
+const router = require("./routes/auth-routes");
 app.use('/', router);
-
-// Passport Flash Error Manager 
-
-app.use(flash());
-passport.use(new LocalStrategy({ 
-  passReqToCallback: true
-}, (req, username, password, next) => {
-   User.findOne({ username }, (err, user) => {
 
 
 module.exports = app;
--- PollenCheckApp/routes/auth-routes.js
+++ PollenCheckApp/routes/auth-routes.js
@@ -1,91 +1,103 @@
 const express = require("express");
-const passport = require("passport");
 const router = express.Router();
-const ensureLogin = require("connect- ensure-login");
-
-
-// Model
-
-const User = require("../models/user.js")
-
-// Bcrypt
-
+const passport = require("passport");
+const ensureLogin = require("connect-ensure-login");
+const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
-// SIGNUP ROUTES
-// ======================
-router.get('/signup', (req, res, next) => {
-    res.render("auth/signup.hbs");
+ 
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
 });
-
+ 
 router.post("/signup", (req, res, next) => {
-    // takes the UserName + Password from the BodyParser 
-    const username = req.body.username;
-    const password = req.body.password;
-
-    // Signup Logic
-    if (username === "" || password === "") {
-        res.render("auth/signup.hbs", {message: "Signup is only possible with a Username and a Password"});
-        return;
+  const username = req.body.username;
+  const password = req.body.password;
+ 
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+ 
+  User.findOne({ username })
+  .then(user => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
     }
-
-    User.findOne({ username })
-    .then(user => {
-        if(user !== null) {
-            res.render("auth/signup.hbs", {message: "This Username belongs to someone else, choose a different Username"});
-            return;
-        }
-
+ 
     const salt = bcrypt.genSaltSync(bcryptSalt);
     const hashPass = bcrypt.hashSync(password, salt);
+ 
     const newUser = new User({
-        username,
-        password: hashPass
+      username,
+      password: hashPass
     });
-
+ 
     newUser.save((err) => {
-        if (err) {
-            res.render("auth/signup.hbs", {message: "Something went wrong"});
-        }   else {
-            res.redirect('/');
-        }
-    })
-    })
-    .catch(error => {
-        next(error)
-    })
+      if (err) {
+        res.render("auth/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/profile-setup");
+      }
+    });
+  })
+  .catch(error => {
+    next(error)
+  })
 });
-// ======================
 
 
-// PASSPORT LOGIN ROUTES
-// ======================
 router.get("/login", (req, res, next) => {
-    res.render("auth/login");
+  res.render("auth/login");
 });
-
+ 
 router.post("/login", passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-    failureFlash: true,
-    passReqToCallback: true
+  successRedirect: "/private-page",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
 }));
-// ======================
-// PASSPORT LOGOUT ROUTES
-// ======================
 
-router.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/login");
-});
-
-
+//Authentication page
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-    res.render("private.hbs", { user: req.user });
+  res.render("private", { user: req.user });
 });
 
+//LOGOUT
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+}); 
 
+
+// PROFILE SETUP PAGE
+
+router.get("/profile-setup", (req, res) => {
+  res.render("profile-setup");
+}); 
+
+// router.post("/profile-setup", (req, res, next) => {
+//   const name = req.body.username;
+//   const zipcode = req.body.zipcode;
+ 
+//   if (zipcode >= 6) {
+//     res.render("profile-setup", { message: "Zipcode is incorrect" });
+//     return;
+//   }
+
+//     User.save((err) => {
+//       if (err) {
+//         res.render("profile-setup", { message: "Something went wrong" });
+//       } else {
+//         res.redirect("/profile-setup");
+//       }
+//     });
+//   })
+//   .catch(error => {
+//     next(error)
+//   })
 
 
 module.exports = router;
